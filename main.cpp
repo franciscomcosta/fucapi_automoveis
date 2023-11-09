1ea89c2013c836c05f1320ade02441d9e1ac59ac
unknown <franciscomoreira009@gmail.com>
2023-11-09 20:49:25 -0300

    correção de keys

--- main.cpp
+++ main.cpp
@@ -1,39 +1,91 @@
 #include "main.h"
 
 #define carsTable "carros.csv"
-#define keys "keys.csv"
+#define keysFile "keys.csv"
+#define order 3
 
 using namespace std;
 
 //ios::app para adicionar conteúdo
 
 
-class Carro {
+class Car {
 public:
     string marca;
     string modelo;
     int ano;
     double preco;
-    string numeroSerie;
+    int serialNumber;
 };
 
+class BTreeNode {
+    public:
+        vector<string> keys;
+        vector<BTreeNode*> children;
+
+        BTreeNode() {}
+
+        // Função para buscar uma chave na árvore B
+        bool search(const string& key) {
+            auto it = lower_bound(keys.begin(), keys.end(), key);
+
+            if (it != keys.end() && *it == key) {
+                cout << "Registro encontrado para a chave: " << key << endl;
+                writeCar(key);  
+                return true;
+            }
+
+            if (children.empty()) {
+                cout << "Registro não encontrado para a chave: " << key << endl;
+                return false;
+            }
+
+            int index = it - keys.begin();
+            return children[index]->search(key);
+        }
+
+        void writeCar(const string& chave) {
+        ifstream carFile(carsTable);
+        if (carFile.is_open()) {
+            string linha;
+            while (getline(carFile, linha)) {
+                if (linha.find(chave) != string::npos) {
+                    cout << "Informações do Carro:\n" << linha << endl;
+                    break;  // A chave é única, então podemos encerrar a busca
+                }
+            }
+            carFile.close();
+        } else {
+            cerr << "Erro ao abrir o arquivo de carros." << endl;
+        }
+    }
+};
 
 //funções da biblioteca files
 void setup(void);
 void createFile(string fileName);
 bool fileExist(string path);
+vector<string> readKeys();
 
 //funções da biblioteca interface
 int showMenu(void);
-int searchCar(void);
-string writeCar(const Carro carro, int index);
+void searchCar(void);
+string writeCar(const Car& carro);
+bool checkSize(int&input, int size);
 
+//função para cadastrar carro
+void cadCar(Car& novoCarro);
+
+//pesquisa
+bool verifyKeys(BTreeNode* root, const string& key);
 
 ofstream file;
 
 int main() {
 
     int option;
+
+  
 
     setlocale(LC_ALL, "pt_BR.utf8");
     setup();
@@ -49,7 +101,15 @@ int main() {
             searchCar();
             break;
         case 2:
-            //writeCar();
+            {
+                Car newCar;
+                cadCar(newCar);
+                cout << writeCar(newCar) << endl;
+                system("pause");
+                system("cls");
+                //writeCar();
+            }
+            
             break;
         case 3:
             searchCar();
@@ -59,6 +119,11 @@ int main() {
             break;
         case 5:
             state = false;
+            break;
+        case 6:
+            cout << "Escolha uma opção válida" << endl;
+            system("Pause");
+            system("cls");
             break;
         
         default:
@@ -109,28 +174,107 @@ int showMenu(void){
     cout << "4 - Listar" << endl;
     cout << "5 - Sair" << endl;
     cout << "Digite a opção escolhida: " << '\n';
-    cin >> choise;
-    return choise;
-}
-
-int searchCar(void){
-
-}
-
-string writeCar(const Carro carro, int index){
     try{
-        fstream file(carsTable, ios::in | ios::out | ios::binary);
-        
-        if(!file.is_open()){
-             throw runtime_error("Falha ao abrir o arquivo. Reinciar sistema!");
+        if(cin >> choise){
+            return choise;
+        }else return 6;
+    }catch(const exception& e){
+        return 6;
+    }
+    
+}
+
+void searchCar(void){
+    string sn;
+    cout << "Digite o SN do carro que deseja encontrar" << endl;
+    cin >> sn;
+    BTreeNode* rootNode = new BTreeNode();
+    vector<string> primaryKeys = readKeys();
+    rootNode->keys = primaryKeys;
+    verifyKeys(rootNode, sn);
+}
+
+string writeCar(const Car& carro) {
+    try {
+        ofstream file(carsTable, ios::app); // Abre o arquivo em modo de adição (append)
+
+        if (!file.is_open()) {
+            throw runtime_error("Falha ao abrir o arquivo CSV. Reiniciar sistema!");
         }
 
-        size_t sizeRegister = sizeof(Carro) * index;
-        file.seekp(sizeRegister);
-        file.write(reinterpret_cast<const char*>(&carro), sizeof(Carro));
+        // Escreve os dados do carro no arquivo CSV
+        file << carro.marca << "," << carro.modelo << "," << carro.ano << "," << carro.preco << "," << carro.serialNumber << "\n";
+
         file.close();
+
+        ofstream keys(keysFile, ios::app);
+        if (!keys.is_open()) {
+            throw runtime_error("Falha ao abrir o arquivo de chaves primárias. Reiniciar o sistema!");
+        }
+
+        // Adicionar a chave primária (número de série) ao arquivo de chaves
+        keys << carro.serialNumber << endl;
+        keys.close();
+
+        
         return "Carro adicionado com sucesso!";
-    }catch(const exception& e){
-        return "Erro ao inserir carro." + string(e.what());
+    } catch (const exception& e) {
+        return "Erro ao inserir carro. " + string(e.what());
     }
+}
+
+
+void cadCar(Car& novoCarro) {
+    cout << "Digite a marca do carro: ";
+    cin >> novoCarro.marca;
+    cout << "Digite o modelo do carro: ";
+    cin >> novoCarro.modelo;
+    cout << "Digite o ano do carro: ";
+    cin >> novoCarro.ano;
+    if(!checkSize(novoCarro.ano, 999)){
+        cout << "Ano inválido. Inicie novamente" << endl;
+        system("pause");
+        system("cls");
+        cadCar(novoCarro);
+    }
+    cout << "Digite o preço do carro: ";
+    cin >> novoCarro.preco;
+    cout << "Digite o número de série do carro: ";
+    cin >> novoCarro.serialNumber;
+    if(!checkSize(novoCarro.serialNumber, 9999)){
+        cout << "Número de serie inválido. Inicie novamente" << endl;
+        system("pause");
+        system("cls");
+        cadCar(novoCarro);
+    }
+}
+
+bool checkSize(int& input, int size){
+    return input > size;
+}
+
+vector<string> readKeys() {
+    ifstream file(keysFile);
+    vector<string> keys;
+
+    if (file.is_open()) {
+        string key;
+        while (getline(file, key)) {
+            keys.push_back(key);
+        }
+        file.close();
+    } else {
+        cerr << "Erro ao abrir o arquivo de chaves primárias." << endl;
+    }
+
+    return keys;
+}
+
+bool verifyKeys(BTreeNode* root, const string& key) {
+    if (!root) {
+        cout << "Árvore B vazia." << endl;
+        return false;
+    }
+    
+    return root->search(key);
 }
--- main.h
+++ main.h
@@ -3,5 +3,7 @@
 
 #include "utils.h"
 #include "files.h"
+#include <vector>
+#include <algorithm>
 
 #endif
